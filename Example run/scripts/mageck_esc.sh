bb2fc9600c0225cf0088a3f79b95180febfb1e26
rgunaratna <33440432+rgunaratna@users.noreply.github.com>
2021-01-13 11:21:34 -0500

    Update mageck_esc.sh

--- Example run/scripts/mageck_esc.sh
+++ Example run/scripts/mageck_esc.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #chmod +x ./mageck_esc.sh && nohup ./mageck_esc.sh > ./mageck_esc.out 2>&1 &
-#module load mageck
-#module load anaconda
-#conda activate mageckenv
+#module load mageck/0.5.9.4
+#module load anaconda && conda activate mageckenv
 
 #count data
 #mageck count -l yusa_library.csv -n escneg --sample-label "plasmid,ESC1" --fastq ERR376998.subsample.fastq  ERR376999.subsample.fastq
